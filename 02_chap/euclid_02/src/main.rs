2a154ef81b8f968b66768c0fb88e0ff332230d85
disharjayanth <dishuj15@gmail.com>
2021-12-18 20:18:22 +0530

    test fn for gcd

--- 02_chap/euclid_02/src/main.rs
+++ 02_chap/euclid_02/src/main.rs
@@ -14,3 +14,10 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
 fn main() {
     println!("{}", gcd(2, 2))
 }
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 *19), 3 * 11);
+}
