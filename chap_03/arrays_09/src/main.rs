b6e00889840d527baacd426a417e59ed093270d0
disharjayantha <dishuj15@gmail.com>
2021-12-26 02:32:15 +0530

    arrays

---
+++ chap_03/arrays_09/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let lazy_caterer = [1, 2, 3, 4, 7, 11, 16];
+    println!("{:#?}", lazy_caterer);
+
+    let taxonomy = ["Animalia", "Arthropoda", "Insecta"];
+    println!("{:#?}", taxonomy);
+
+    let long_array = [true; 10];
+    println!("{:?}", long_array);
+}
