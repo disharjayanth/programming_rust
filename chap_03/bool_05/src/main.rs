5a2ca235876ef432bb83a30798a6f6491eeac27b
disharjayantha <dishuj15@gmail.com>
2021-12-26 00:16:40 +0530

    few changes

--- chap_03/bool_05/src/main.rs
+++ chap_03/bool_05/src/main.rs
@@ -7,4 +7,13 @@ fn main() {
     } else {
         println!("a = b");
     }
+
+    assert_eq!(false as i32, 0);
+    assert_eq!(true as i32, 1);
+
+    let c = true as i32;
+    println!("{}", c);
+
+    let d = false as i32;
+    println!("{}", d);
 }
