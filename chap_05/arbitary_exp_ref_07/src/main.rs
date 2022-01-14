6040ddd1344bdcb54d0e69b0d2df8fae31e226a2
disharjayantha <dishuj15@gmail.com>
2022-01-15 02:55:59 +0530

    arbitary expression with reference

---
+++ chap_05/arbitary_exp_ref_07/src/main.rs
@@ -0,0 +1,12 @@
+fn factorial(n: usize) -> usize {
+    return (1..n + 1).product();
+}
+
+fn main() {
+    let r = &factorial(6);
+    print!("r: {}", r);
+
+    assert_eq!(*r, 720);
+
+    assert_eq!(r + &1009, 1729);
+}
