89de6b5af5ea1cf1a3cfdf7cc89ffd2d0bfcfc01
disharjayanth <dishuj15@gmail.com>
2022-01-20 00:58:43 +0530

    edit comment

--- chap_05/sharing_mut_14/src/main.rs
+++ chap_05/sharing_mut_14/src/main.rs
@@ -33,7 +33,12 @@ fn main() {
 
     assert_eq!(wave, [0.0, 1.0, 0.0, -1.0]);
 
-    // below code throws error since wave is both mutable and 
-    // immutable at the same time and also because of lifetime
+    // below code throws error because in extend fn, vec: &mut Vec<f64> is only way to reach vector 
+    // or it's elements, the shared reference from slice: &[f64] is another way of reaching elems
+    // which is NOT good, thus violating 2nd rule of MUTABLE ACCESS IS EXCLUSIVE ACESSS.
+    // OR 
+    // rust could also have treated bug as a violation of first rule: since we've shared reference
+    // to wave's elements, the elements and the vec itself are read-only. You can't borrow a mutable
+    // reference to a read-only value.
     // extend(&mut wave, &wave);
 }
