d4f64483b779edd723f77afafbfa498524709e65
disharjayantha <dishuj15@gmail.com>
2022-01-24 00:25:20 +0530

    controlling loop with break, values and labels

--- chap_06/control_loop_06/src/main.rs
+++ chap_06/control_loop_06/src/main.rs
@@ -3,6 +3,13 @@ fn next_line<'a>() -> Option<&'a str> {
     // None
 }
 
+fn trim_comments_and_whitespaces(line: &mut String) -> &mut String {
+    line.retain(|c| !c.is_whitespace());
+    line.retain(|c| c != '\n');
+    line.retain(|c| c != '\\');
+    line
+}
+
 fn main() {
     let answer = loop {
         if let Some(line) = next_line() {
@@ -15,4 +22,21 @@ fn main() {
     };
 
     println!("{answer}");
+
+    let mut input_lines = vec![
+        "Hello    world".to_string(),
+        "Good luck\n".to_string(),
+        " ".to_string(),
+        "thanks a   lot".to_string(),
+    ];
+
+    for line in &mut input_lines {
+        let trimmed = trim_comments_and_whitespaces(line);
+        if trimmed.is_empty() {
+            trimmed.push_str("IS EMPTY!!!");
+            continue;
+        }
+    }
+
+    println!("{:?}", input_lines);
 }
