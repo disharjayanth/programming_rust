40e8924fac3a2821a67e24ed0fc5483db483ebf8
disharjayantha <dishuj15@gmail.com>
2022-01-24 22:18:42 +0530

    return in function

--- chap_06/return_07/src/main.rs
+++ chap_06/return_07/src/main.rs
@@ -1,4 +1,7 @@
-use std::{fs::File, io::Write};
+use std::{
+    fs::File,
+    io::{Read, Write},
+};
 
 fn some_string<'a>() -> &'a str {
     "Hello world!"
@@ -16,5 +19,10 @@ fn main() -> std::io::Result<()> {
     output.write(
         b"package main \n \nimport \"fmt\" \n \nfunc main() { \n \tfmt.Println(\"Hello world\") \n}",
     )?;
+
+    let mut read = File::open("hello.go")?;
+    let mut contents = String::new();
+    read.read_to_string(&mut contents)?;
+    println!("contents of hello.go file: {}", contents);
     Ok(())
 }
