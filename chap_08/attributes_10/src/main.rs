61b613e684e06fb8cbbc89cbcca99fc61eecf4bd
disharjayantha <dishuj15@gmail.com>
2022-03-03 22:01:47 +0530

    attributes and prev library

---
+++ chap_08/attributes_10/src/main.rs
@@ -0,0 +1,9 @@
+#[derive(Debug)]
+#[allow(non_camel_case_types)]
+pub struct git_revspec {}
+
+fn main() {
+    let some_git_revspec = git_revspec {};
+    println!("{:?}", some_git_revspec);
+    println!("Hello, world!");
+}
