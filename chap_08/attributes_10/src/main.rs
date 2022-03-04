ed6bc160e404ed75713b6d42d0ad5ee9a2285a89
disharjayantha <dishuj15@gmail.com>
2022-03-05 00:54:45 +0530

    attributes and test

--- chap_08/attributes_10/src/main.rs
+++ chap_08/attributes_10/src/main.rs
@@ -1,9 +1,20 @@
+#![allow(non_camel_case_types)]
+// #![feature(trace_macros)] // can only be used on experimental version
+
 #[derive(Debug)]
-#[allow(non_camel_case_types)]
 pub struct git_revspec {}
+
+#[derive(Debug)]
+pub struct git_error {}
 
 fn main() {
     let some_git_revspec = git_revspec {};
+    let some_git_error = git_error {};
+
     println!("{:?}", some_git_revspec);
-    println!("Hello, world!");
+    println!("{:?}", some_git_error);
+
+    // trace_macros!(true);
+    // println!("Hello, world!");
+    // trace_macros!(false);
 }
---
+++ chap_08/test_doc_11/src/main.rs
@@ -0,0 +1,42 @@
+use std::num::ParseIntError;
+
+#[test]
+fn math_works() {
+    let x: i32 = 1;
+    assert!(x.is_positive());
+    assert_eq!(x + 1, 2);
+}
+
+#[test]
+fn string_works() {
+    let x = "hello world";
+    assert_eq!(x, "hello world");
+}
+
+#[test]
+fn check_bool() {
+    let x = true;
+    assert_eq!(true, x);
+}
+
+// below test check_num will fail if x != 12
+#[test]
+fn check_num() {
+    let x = 12;
+    assert_eq!(12, x);
+}
+
+#[test]
+#[should_panic(expected = "divide by zero")]
+#[allow(unconditional_panic, unused_must_use)]
+fn test_divide_by_zero_error() {
+    1 / 0;
+}
+
+#[test]
+fn parse() -> Result<(), ParseIntError> {
+    i32::from_str_radix("1024", 10)?;
+    Ok(())
+}
+
+fn main() {}
