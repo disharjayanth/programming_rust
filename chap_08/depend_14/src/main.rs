3524574f5d748b26a702c22e0a60ba31ea7b4cbd
disharjayantha <dishuj15@gmail.com>
2022-03-08 19:02:47 +0530

    more on dependencies

--- chap_08/depend_14/src/main.rs
+++ chap_08/depend_14/src/main.rs
@@ -1,8 +1,19 @@
-use image::GenericImage;
+use image::{GenericImage, Rgb, RgbImage};
 
 fn main() {
     let img = image::open("/Users/disharjayantha/Documents/10-11.jpg").unwrap();
 
     println!("dimensions: {:?}", img.dimensions());
     println!("{:?}", img.color());
+
+    let mut img = RgbImage::new(32, 32);
+
+    for x in 15..=17 {
+        for y in 8..24 {
+            img.put_pixel(x, y, Rgb([255, 0, 0]));
+            img.put_pixel(y, x, Rgb([255, 0, 0]));
+        }
+    }
+
+    img.save("test.png").unwrap();
 }
