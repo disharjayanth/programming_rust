5c1e8703d3e4059ff95d8fecab655d7f30aa8032
disharjayantha <dishuj15@gmail.com>
2022-04-25 21:01:13 +0530

    box rc

---
+++ chap_09/box_rc_09/src/main.rs
@@ -0,0 +1,61 @@
+#[derive(Debug)]
+pub struct Queue {
+    older: Vec<char>,
+    younger: Vec<char>,
+}
+
+impl Queue {
+    pub fn push(&mut self, c: char) {
+        self.younger.push(c);
+    }
+
+    pub fn pop(&mut self) -> Option<char> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+
+            // Bring the elements in younger over to older, and put them in
+            // the promised order.
+            use std::mem::swap;
+            swap(&mut self.older, &mut self.younger);
+            self.older.reverse();
+        }
+
+        self.older.pop()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.older.is_empty() && self.younger.is_empty()
+    }
+
+    pub fn split(self) -> (Vec<char>, Vec<char>) {
+        (self.older, self.younger)
+    }
+}
+
+fn main() {
+    let mut q = Queue {
+        older: Vec::new(),
+        younger: Vec::new(),
+    };
+
+    q.push('0');
+    q.push('1');
+    assert_eq!(q.pop(), Some('0'));
+
+    q.push('=');
+    assert_eq!(q.pop(), Some('1'));
+    assert_eq!(q.pop(), Some('='));
+    assert_eq!(q.pop(), None);
+
+    println!("{:?}", q);
+
+    assert!(q.is_empty());
+
+    q.push('P');
+    q.push('D');
+
+    let (older, younger) = q.split();
+    println!("{:?} {:?}", older, younger);
+}
