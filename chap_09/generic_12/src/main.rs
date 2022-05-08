3d8fcff0e1218459b57ac86b52de9f8f1d2c506d
disharjayantha <dishuj15@gmail.com>
2022-05-08 20:52:27 +0530

    generic structs

---
+++ chap_09/generic_12/src/main.rs
@@ -0,0 +1,44 @@
+use std::mem::swap;
+
+pub struct Queue<T> {
+    older: Vec<T>,
+    younger: Vec<T>,
+}
+
+impl<T> Queue<T> {
+    pub fn new() -> Queue<T> {
+        Queue {
+            older: Vec::new(),
+            younger: Vec::new(),
+        }
+    }
+
+    pub fn push(&mut self, t: T) {
+        self.younger.push(t);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+
+            swap(&mut self.older, &mut self.younger);
+            self.older.reverse();
+        }
+
+        self.older.pop()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.older.is_empty() && self.younger.is_empty()
+    }
+
+    pub fn split(self) -> (Vec<T>, Vec<T>) {
+        (self.older, self.younger)
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
