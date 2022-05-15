736f1dbbd84a1c2d7d9a9429548623138a557438
disharjayantha <dishuj15@gmail.com>
2022-05-16 04:34:07 +0530

    struct traits and inter mutability

---
+++ chap_09/inter_mut_16/src/main.rs
@@ -0,0 +1,9 @@
+pub struct SpirderRobot {
+    species: String,
+    web_enabled: bool,
+    leg_devices: [fd::FileDesc; 8],
+}
+
+fn main() {
+    println!("Hello, world!");
+}
--- chap_09/struct_trait_15/src/main.rs
+++ chap_09/struct_trait_15/src/main.rs
@@ -5,5 +5,18 @@ struct Point {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let p = Point { x: 1.0, y: 2.0 };
+
+    println!("p: {:?}", p);
+
+    let q = p;
+
+    println!("p: {:?}", p);
+    println!("q: {:?}", q);
+
+    if p.x > p.y {
+        println!("x is greater than y");
+    } else {
+        println!("y is greater than x");
+    }
 }
