34186848b96aa2ca478b2a7b13d8e524cbe3763c
disharjayantha <dishuj15@gmail.com>
2022-05-17 22:10:52 +0530

    rc counter

--- chap_09/inter_mut_16/src/main.rs
+++ chap_09/inter_mut_16/src/main.rs
@@ -1,9 +1,29 @@
-pub struct SpirderRobot {
-    species: String,
-    web_enabled: bool,
-    leg_devices: [fd::FileDesc; 8],
-}
+// pub struct SpirderRobot {
+//     species: String,
+//     web_enabled: bool,
+//     leg_devices: [fd::FileDesc; 8],
+// }
+
+// pub struct SpiderSenses {
+//     robot: Rc<SpirderRobot> ,
+//     eyes: [Camera; 32] ,
+//     motion: Accel
+// }
+
+use std::rc::Rc;
 
 fn main() {
-    println!("Hello, world!");
+    let mut rc_examples = "Rc examples".to_string();
+
+    let rc_a = Rc::new(rc_examples);
+    println!("Reference Count of rc_a: {}", Rc::strong_count(&rc_a));
+
+    let rc_b = Rc::clone(&rc_a);
+    println!("Reference Count of rc_b: {}", Rc::strong_count(&rc_b));
+
+    rc_examples = "changes".to_string();
+
+    println!("{}", rc_examples);
+    println!("{}", rc_a);
+    println!("{}", rc_b);
 }
