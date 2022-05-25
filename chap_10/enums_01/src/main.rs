8a327835024050b552a1b733892cf77d015afb61
disharjayantha <dishuj15@gmail.com>
2022-05-25 22:16:48 +0530

    enums

--- chap_10/enums_01/src/main.rs
+++ chap_10/enums_01/src/main.rs
@@ -1,4 +1,12 @@
 use std::cmp::Ordering;
+use std::mem::size_of;
+
+#[derive(Debug)]
+enum HttpStatus {
+    Ok = 200,
+    NotModified = 304,
+    NotFound = 404,
+}
 
 fn compare(n: i32, m: i32) -> Ordering {
     if n < m {
@@ -12,4 +20,8 @@ fn compare(n: i32, m: i32) -> Ordering {
 
 fn main() {
     println!("{:?}", compare(10, 2));
+
+    assert_eq!(size_of::<Ordering>(), 1);
+
+    assert_eq!(HttpStatus::Ok as i32, 200);
 }
