62cfeb3a62b7bf33e36bbf78e3f91aaa0e39cd00
disharjayantha <dishuj15@gmail.com>
2022-05-25 21:30:27 +0530

    chap 10

---
+++ chap_10/enums_01/src/main.rs
@@ -0,0 +1,15 @@
+use std::cmp::Ordering;
+
+fn compare(n: i32, m: i32) -> Ordering {
+    if n < m {
+        Ordering::Less
+    } else if n > m {
+        Ordering::Greater
+    } else {
+        Ordering::Equal
+    }
+}
+
+fn main() {
+    println!("{:?}", compare(10, 2));
+}
