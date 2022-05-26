e4a0f26e17fff5ebe320d2cd3e8891225c97f671
disharjayantha <dishuj15@gmail.com>
2022-05-26 21:01:37 +0530

    enums

--- chap_10/enums_01/src/main.rs
+++ chap_10/enums_01/src/main.rs
@@ -18,10 +18,51 @@ fn compare(n: i32, m: i32) -> Ordering {
     }
 }
 
+fn http_status(n: u32) -> Option<HttpStatus> {
+    match n {
+        200 => Some(HttpStatus::Ok),
+        304 => Some(HttpStatus::NotModified),
+        404 => Some(HttpStatus::NotFound),
+        _ => None,
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum TimeUnit {
+    Seconds,
+    Minutes,
+    Hours,
+    Days,
+    Months,
+    Years,
+}
+
+impl TimeUnit {
+    fn plural(self) -> &'static str {
+        match self {
+            TimeUnit::Seconds => "seconds",
+            TimeUnit::Minutes => "minutes",
+            TimeUnit::Hours => "hours",
+            TimeUnit::Days => "days",
+            TimeUnit::Months => "months",
+            TimeUnit::Years => "years",
+        }
+    }
+
+    fn singular(self) -> &'static str {
+        self.plural().trim_end_matches('s')
+    }
+}
+
 fn main() {
     println!("{:?}", compare(10, 2));
 
     assert_eq!(size_of::<Ordering>(), 1);
 
     assert_eq!(HttpStatus::Ok as i32, 200);
+
+    let sec = TimeUnit::Seconds;
+
+    println!("{}", sec.plural());
+    println!("{}", sec.singular());
 }
