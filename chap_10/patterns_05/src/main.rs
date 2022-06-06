eb3ae7bcbae6d4c9ff8a473cfbe626ac9404c1be
disharjayantha <dishuj15@gmail.com>
2022-06-06 22:49:47 +0530

    patterns

--- chap_10/patterns_05/src/main.rs
+++ chap_10/patterns_05/src/main.rs
@@ -13,6 +13,14 @@ enum RoughTime {
     InTheFuture(TimeUnit, u32),
 }
 
+fn rough_time_to_english(rt: RoughTime) -> String {
+    match rt {
+        RoughTime::InThePastTime(units, count) => format!("{} {} ago", count, units.plural()),
+        RoughTime::JustNow => format!("just now"),
+        RoughTime::InTheFuture(units, count) => format!("{} {} from now", count, units.plural()),
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
